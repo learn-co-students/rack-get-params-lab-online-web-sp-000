bc60d34fdbb8127885b415d7b081a4dd59ffa120
Bryce Eadie <bryce.eadie@gmail.com>
2018-11-05 07:44:59 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,3 +1,7 @@
+
+
+# Create a new route called /add that takes in a GET param with the key item. This should check to see if that item is in @@items and then add it to the cart if it is. Otherwise give an error
+
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
@@ -13,6 +17,22 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+    elsif req.path.match(/add/)
+      search_term = req.params["item"]
+      if @@items.include?(search_term)
+        @@cart << search_term
+        resp.write "added #{search_term}"
+      else
+        resp.write "We don't have that item"
+      end
     else
       resp.write "Path Not Found"
     end
