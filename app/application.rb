83008ec6ac50de2005a54d2445b8851028766599
Ariel <arielord@buffalo.edu>
2019-03-22 02:22:44 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -21,8 +21,13 @@ class Application
         @@cart.each {|item| resp.write "#{item}\n"}
       end
     elsif req.path.match(/add/)
-      search_term = req.params["q"]
-      @@cart << search_term unless handle_search(search_term).include?("Couldn't")
+      search_term = req.params["item"]
+      if @@items.include?(search_term)
+        @@cart << search_term
+        resp.write "added #{search_term}"
+      else
+        resp.write "We don't have that item #{search_term}"
+      end
     else
       resp.write "Path Not Found"
     end
