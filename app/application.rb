173c4791db61fac2f42f486515e7ba20cfac585e
Yingqi Chen <chanwk18@hotmail.com>
2019-06-08 23:45:05 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-
+    #binding.pry
     if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
