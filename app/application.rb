f79d0424303ce1f349478a2af6eea647eeaba9a8
Blythe Pollard <blytheepollard@gmail.com>
2019-11-20 23:45:34 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,6 @@
 class Application
 
+  @@cart = ["Apples, Oranges"]
   @@items = ["Apples","Carrots","Pears"]
 
   def call(env)
@@ -13,6 +14,14 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.length > 0
+      @@cart.each do |item|
+      resp.write "#{item}\n"
+      end
+      else
+      resp.write "Your cart is empty"
+      end
     else
       resp.write "Path Not Found"
     end
