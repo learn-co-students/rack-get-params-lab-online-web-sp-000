4223b0ebd0a45d48f5564078691d4c1960baef32
David Cantoran <davidcantoran36@gmail.com>
2020-02-26 00:31:25 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,29 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+    
+    if req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      elsif !@@cart.empty? 
+        @@cart.each {|i| resp.write "#{i}\n"}
+      end 
+    end 
+    
+    if req.path.match(/add/)
+      added_item = req.params["item"]
+      if @@items.include?(added_item)
+        @@cart << added_item
+        resp.write "added #{added_item}"
+      else 
+        resp.write "We don't have that item"
+      end 
+    end 
 
     if req.path.match(/items/)
       @@items.each do |item|
