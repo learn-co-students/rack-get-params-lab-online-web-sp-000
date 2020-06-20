1fd2e8b5d3ab5ebce4acbaeb4bdd8ac19f7b1321
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-20 02:58:27 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -5,21 +5,22 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    req = Rack::Request.new(env)
+    req = Rack::Request.new(env) #env part of our call function has all info stored in the request
 
-    if req.path.match(/items/)
+    if req.path.match(/items/) #allows us to see all of our items
       @@items.each do |item|
         resp.write "#{item}\n"
       end
-    elsif req.path.match(/search/)
+    elsif req.path.match(/search/) #GET parameters key/value pairs.
+      #search route that accepts a GET parameter w/ the key q.
 
-      search_term = req.params["q"]
+      search_term = req.params["q"] #key = q, value = apples, carrots, or pears
 
       resp.write handle_search(search_term) #not sure what this does
 
       def handle_search(search_term)
-        if @@items.include?(search_term)
-          return "#{search_term} is one of our items"
+        if @@items.include?(search_term) #?
+          return "#{search_term} is one of our items" #we can do different things depending on the search path
         else
           return "Couldn't find #{search_term}"
         end
