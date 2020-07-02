c5cb5f29296aa178d852affc4068af3692a6f2cf
ting682 <tchung682@gmail.com>
2020-07-02 11:19:53 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
@@ -13,6 +14,25 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.size == 0
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+    elsif req.path.match(/add/)
+
+      get_item = req.params["item"]
+
+      if !@@items.include?(get_item)
+        resp.write "We don't have that item"
+      else
+        @@cart << get_item
+        resp.write "added #{get_item}"
+      end
+
     else
       resp.write "Path Not Found"
     end
