d2cba7b2821526c72fa444b5b90c8ff6057e490b
Nat McConnaughay <natmcconnaughay@gmail.com>
2020-10-06 16:22:50 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -13,6 +13,11 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      resp.write display_cart
+    elsif req.path.match(/add/)
+      item = req.params["item"]
+      resp.write add_item(item)
     else
       resp.write "Path Not Found"
     end
@@ -27,4 +32,22 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+
+  def display_cart
+    if @@cart.empty?
+      return "Your cart is empty"
+    else
+      @@cart.join("\n")
+    end
+  end
+
+  def add_item(item)
+    if @@items.include?(item)
+      @@cart << item
+      "added #{item}"
+    else
+      "We don't have that item"
+    end
+  end
+
 end
